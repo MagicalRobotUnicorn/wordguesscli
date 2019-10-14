565665f827f50ed6eb3273111e44c7e1a84b0c3c
John Weber <johnhenryweber@gmail.com>
2019-10-13 22:16:09 -0700

    Wireframe Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hello World...');
+var inquirer = require("inquirer");
+
+// Array of possible answers
+
+// Game object
+
+var Game = function(){
+  var victory = false;
+
+  var words = ['Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Jimi Hendrix', 'Fleetwood Mac', 'Depeche Mode', 'The Cure', 'Nirvana', 'Metallica', 'Kraftwerk', 'The Pixies'];
+
+  var gameWords = [];
+
+  function startGame(){
+
+  }
+
+  function guessLetter(letter){
+
+  }
+
+  function checkVictory(){
+
+  }
+
+  function convertWord(word){
+
+  }
+
+  function repeatGame(response){
+
+  }
+}
